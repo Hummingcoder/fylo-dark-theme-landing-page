172b7d579cf20bbe55794b7d04dbcdbb59922560
Vishal D. <dwivedivishal0702@gmail.com>
2025-01-11 12:41:39 +0530

    small fixes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "fylo-dark-theme-landing-page",
+  base: "/fylo-dark-theme-landing-page",
   plugins: [react()],
 });
